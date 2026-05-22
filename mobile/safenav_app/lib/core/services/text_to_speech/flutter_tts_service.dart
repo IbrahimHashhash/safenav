644b6d305ecca55e0db8d9e677c03288fec1fccc
IbrahimHashhash <ibrahimhash0594542602@gmail.com>
2026-05-22 16:37:18 +0300

    feat: add tts config

--- mobile/safenav_app/lib/core/services/text_to_speech/flutter_tts_service.dart
+++ mobile/safenav_app/lib/core/services/text_to_speech/flutter_tts_service.dart
@@ -1,16 +1,24 @@
 import 'package:flutter_tts/flutter_tts.dart';
-
+import 'tts_config.dart';
 import 'tts_service.dart';
 
 class FlutterTtsService implements TtsService {
   final FlutterTts _tts;
+  final TtsConfig config;  // ← missing this line
 
-  FlutterTtsService(this._tts);
+  FlutterTtsService(this._tts, {this.config = const TtsConfig()});
+
+  Future<void> _applyConfig() async {
+    await _tts.setLanguage(config.language);
+    await _tts.setSpeechRate(config.speechRate);
+    await _tts.setPitch(config.pitch);
+    await _tts.setVolume(config.volume);
+  }
 
   @override
   Future<void> speak(String text) async {
     await _tts.stop();
-
+    await _applyConfig();  // ← also wire this in
     await _tts.speak(text);
   }
 

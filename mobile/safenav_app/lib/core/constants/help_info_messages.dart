6be437f3c9c91aa2982d06a2948d5bcc49346a67
Mays Alreem <hroubmays@gmail.com>
2026-06-21 23:22:51 +0300

    feat: add start/stop detection to commands list

--- mobile/safenav_app/lib/core/constants/help_info_messages.dart
+++ mobile/safenav_app/lib/core/constants/help_info_messages.dart
@@ -1,4 +1,8 @@
 class HelpInfoMessages {
   static const availableCommands =
-      'Available commands are: navigate to a destination, list faculties, list libraries, list cafeterias, full list, repeat, and more info';
+      'Available commands are: navigate to a destination, '
+      'start navigation, stop navigation, '
+      'start detection, stop detection, '
+      'list faculties, list libraries, list cafeterias, full list, '
+      'repeat, and more info';
 }
